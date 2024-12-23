8b7cfdae49a5657647cf6d7f01c9df5056135396
Hao Tang <tanghaosjtu@gmail.com>
2024-12-22 19:13:24 -0500

    Test llm2prog

--- llm2prog.py
+++ llm2prog.py
@@ -1,69 +1,25 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import os, os.path as osp, sys
-sys.path.append(osp.abspath(osp.join(osp.dirname(__file__), '..', '..')))
+import argparse
 
-import copy
-import itertools
-from pprint import pprint
+from proggen.data import Dataset
+from proggen.llm2prog import LLM2Prog, template_OriginalScreenshot_AnnotatedContour_WithoutSemanticLabels
 
-import random
-import numpy as np
-import gymnasium as gym
-
-from Box2D import b2World
-
-from cont_world_coder.envs.render import PygameRender, OpenCVRender
-from cont_world_coder.envs.lunar_lander.lunar_lander import LunarLanderWrapper
-from cont_world_coder.envs.cartpole.cartpole import CartPoleWrapper
-from cont_world_coder.envs.phyworld.two_ball_collision import PhyworldTwoBallCollisionWrapper
-from cont_world_coder.envs.phyworld.one_ball_uniform_motion import PhyworldOneBallUniformMotionWrapper
-from cont_world_coder.envs.phyworld.one_ball_parabola import PhyworldOneBallParabolaWrapper
-from cont_world_coder.envs.phyworld_phyre.phyworldphyre import PhyworldPhyreWrapper
-
-from cont_world_coder.dynamics.llm2prog import LLM2Prog, template_OriginalScreenshot_AnnotatedContour_WithoutSemanticLabels
-from cont_world_coder.dynamics.llm2prog import template_OriginalScreenshot_AnnotatedContour_WithoutSemanticLabels_StackVideo
-from cont_world_coder.dynamics.dsl import code2prog
-
-def test_llm2prog_cartpole():
-    seed = 2
-    random.seed(seed)
-    np.random.seed(seed)
-    # env = CartPoleWrapper(gym.make('CartPole-v1', render_mode='rgb_array',))
-    # env = LunarLanderWrapper(gym.make('LunarLander-v3', render_mode='rgb_array',))
-    # env = PhyworldTwoBallCollisionWrapper(gym.make('PhyworldTwoBallCollision-v0', render_mode='rgb_array',))
-    # env = PhyworldOneBallUniformMotionWrapper(gym.make('PhyworldOneBallUniformMotion-v0', render_mode='rgb_array',))
-    # env = PhyworldOneBallParabolaWrapper(gym.make('PhyworldOneBallParabola-v0', render_mode='rgb_array',))
-    env = PhyworldPhyreWrapper(gym.make('PhyworldPhyre-v0', render_mode='rgb_array',))
-    render = OpenCVRender(env.unwrapped.SCREEN_WIDTH, env.unwrapped.SCREEN_HEIGHT, env.unwrapped.PPM)
-
-    obs, info = env.reset(seed=0)
-    trajs = []
-    screenshots = []
-    for t in range(100):
-        action = 1 if isinstance(env, CartPoleWrapper) else 0 # Do thing action
-        obs, reward, terminated, truncated, info = env.step(action)
-        trajs.append(obs)
-        screenshots.append(env.render())
-        # screenshots.append(render.render(obs))
-        # import matplotlib.pyplot as plt
-        # plt.imshow(screenshots[-1])
-        # plt.show()
-        if terminated or truncated:
-            break
-    env.close()
-
-    screenshots = np.array(screenshots)
-
-    # llm2prog = LLM2Prog(template_OriginalScreenshot_AnnotatedContour_WithoutSemanticLabels)
-    llm2prog = LLM2Prog(template_OriginalScreenshot_AnnotatedContour_WithoutSemanticLabels_StackVideo)
-    code = llm2prog(trajs, screenshots, env.unwrapped.PPM)
-    # code = llm2prog(trajs, screenshots, env.unwrapped.PPM)
-    # code = llm2prog(trajs, screenshots, env.unwrapped.PPM)
+def llm2prog_main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, default='collision')
+    parser.add_argument('--split', type=str, default='train')
+    args = parser.parse_args()
+    print(args)
+    dataset = Dataset(args.dataset, args.split, seed=0,)
+    data = dataset[0]
+    trajs, screenshots = data['trajs'], data['frames']
+    llm2prog = LLM2Prog(template_OriginalScreenshot_AnnotatedContour_WithoutSemanticLabels)
+    code = llm2prog(trajs, screenshots, screenshots.shape[-2]/10.)
     print(code)
 
 if __name__ == '__main__':
-    test_llm2prog_cartpole()
+    llm2prog_main()
 
 
--- proggen/llm2prog/llm2prog.py
+++ proggen/llm2prog/llm2prog.py
@@ -11,7 +11,8 @@ from matplotlib import colormaps
 import cv2
 import PIL.Image as Image
 
-from .llm import LLM
+from .dsl import DSLDoc, DSLOneshotExample
+from ..utils.llm import LLM
 
 class LLM2Prog:
     def __init__(self, template, llm_hyperparameters = {
@@ -54,23 +55,6 @@ class LLM2Prog:
                 code += line + '\n'
         assert code, 'No code block found'
         return code.strip()
-
-# Write documenation for the DSL here, including can_overlap, add_body, and
-# add_joint. Describing their arguments, return values, and functionality.
-DSLDoc = '''
-can_overlap(s1, s2) - Declare that shapes s1 and s2 can overlap.
-body = add_body(shapes, body_type) - Add a body with a list of shapes and a body type (static, dynamic, kinematic).
-joint = add_joint(body1, body2, joint_type) - Add a joint between two bodies with a joint type (revolute, prismatic, distance, weld).
-'''.strip()
-DSLOneshotExample = '''
-# This example code creates a simple environment with a static body and two dynamic bodies connected by a revolute joint. The static body, body1, has one shape, shape1. The first dynamic body, body2, has one shape, shape2. The second dynamic body, body3, has two shapes, shape3 and shape4. The two dynamic bodies are connected by a revolute joint, joint1. shape2 and shape3 can overlap, and shape2 and shape4 can overlap.
-can_overlap('shape2', 'shape3')
-can_overlap('shape2', 'shape4')
-body1 = add_body(['shape1'], 'static')
-body2 = add_body(['shape2'], 'dynamic')
-body3 = add_body(['shape3', 'shape4'], 'dynamic')
-joint1 = add_joint(body2, body3, 'revolute')
-'''.strip()
 
 SYSTEM_MESSAGE = '''
 Your goal is to write a program to simulate the environment that the agent is interacting with. Specially, we know that the environment is a 2D world following physical laws, so we wrap up the low-level physical laws into a simulator. Your task is to set up the simulator with the right constraints such as which shapes belong to the same body, if two shapes can overlap with each other or not (can_overlap means they do not collide), and if two bodies are connected by a joint. The simulator will then handle all the details including the shape shapes, positions, angle, and so on. You should ONLY focus on the high-level constraints. The simulator will handle the rest.
@@ -240,7 +224,6 @@ def annotate_image_contour(image, state, shape_names, PPM):
             if overlap_area < 1:
                 # assert False
                 break
-        break; #TODO: remove this line
 
     for i, (text_box, config) in enumerate(zip(notations, smallest_overlap_config)):
         shape_name = text_box[0]
@@ -285,14 +268,12 @@ def template_OriginalScreenshot_AnnotatedContour_WithoutSemanticLabels_StackVide
     shapes = get_shape_names(trajs_as_text)
     anonymous_shapes = [f'shape{i}' for i in range(len(shapes))]
     frame_index = rng.integers(len(trajs_as_text))
-    frame_index = 0
     # annotated_screenshot = annotate_image_boundingbox(trajs_as_video[frame_index], trajs_as_text[frame_index], {k:v for k,v in zip(shapes, anonymous_shapes)}, PPM)
-    # annotated_screenshot = annotate_image_contour(trajs_as_video[frame_index], trajs_as_text[frame_index], {k:v for k,v in zip(shapes, anonymous_shapes)}, PPM)
-    annotated_screenshot = annotate_image_contour(trajs_as_video[frame_index], trajs_as_text[frame_index], {k:v for k,v in zip(shapes, shapes)}, PPM)
+    annotated_screenshot = annotate_image_contour(trajs_as_video[frame_index], trajs_as_text[frame_index], {k:v for k,v in zip(shapes, anonymous_shapes)}, PPM)
     import matplotlib.pyplot as plt
     plt.imshow(annotated_screenshot)
     plt.show()
-    assert False
+    # assert False
     annotated_screenshot = base64.b64encode(cv2.imencode('.png', annotated_screenshot)[1]).decode('utf-8')
     # trajs_as_video = [annotate_image_contour(trajs_as_video[fi], trajs_as_text[fi], {k:v for k,v in zip(shapes, anonymous_shapes)}, PPM) for fi in range(len(trajs_as_video))]
     trajs_image = build_trajs_image(trajs_as_video, rng)
